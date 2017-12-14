8b23c443f71611987a9d70535d1b9bb6d542a8c2
Jianyu Zuo <zuowings@gmail.com>
2017-12-13 23:06:56 -0600

    Chap5 Implementation Hiding
    Basic as Classes

---
+++ Chapter05.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jianyu Zuo on 12/13/17.
+//
+
+#include "Chapter05.h"
---
+++ Chapter05.h
@@ -0,0 +1,14 @@
+//
+// Created by Jianyu Zuo on 12/13/17.
+//
+
+#ifndef THINKINGINCPP_CHAPTER5_H
+#define THINKINGINCPP_CHAPTER5_H
+
+
+class Chapter5 {
+
+};
+
+
+#endif //THINKINGINCPP_CHAPTER5_H
